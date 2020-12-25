8d27236cc3ac3010e813d0a5b31719d0ae7c2796
fercho1997 <70421735+fercho1997@users.noreply.github.com>
2020-12-25 15:05:51 -0500

    Update wifi_con_led.ino

--- wifi_con_led.ino
+++ wifi_con_led.ino
@@ -1,18 +1,19 @@
+/// Librerias para descargar
 #define BLYNK_PRINT Serial
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
-char auth[] = " 87_40COVg9BhsBuZdv-QCNQuvopDYnUB";
-char ssid[] = "UbeeF53C"; 
-char pass[] = "5BADF0F53C"; 
+char auth[] = "Clave del TOKEN";
+char ssid[] = "Nombre de la red wifi"; 
+char pass[] = "Contraseña de la red wifi"; 
 
 void setup()
 {
   Serial.begin(9600);
-  Blynk.begin(auth, ssid, pass);
+  Blynk.begin(auth, ssid, pass); //comunicacion con  la app Blynk
 }
 
 void loop()
 {
-  Blynk.run();
+  Blynk.run();  //conecion con  la app Blynk
 }
