336c1b3818579afb2a44b216dfac0f6a5a1a87c7
fercho1997 <70421735+fercho1997@users.noreply.github.com>
2020-12-28 16:38:05 -0500

    Update aplaudir_controlar_luz.ino

--- aplaudir_controlar_luz.ino
+++ aplaudir_controlar_luz.ino
@@ -1,10 +1,10 @@
 
 
-int Mosonido = 2;       
-int rele = 3;        
-int estado=0;
-int encender=0;
-int anterior=0;
+int Mosonido = 2;       // pin 2 modulo de sonido 
+int rele = 3;        // pin 3 control del rele
+int estado=0;     // comparar si mi estado es alto o bajo
+int encender=0;    // si mi estado es alto encender el rele 
+int anterior=0;    // nos regresa al estado de apagdao 
 
 void setup(){
   pinMode(2, INPUT);      // pin 2 como entrada
@@ -13,21 +13,22 @@ void setup(){
 
 
 void loop(){
-   estado = digitalRead(2);
+ 
+ estado = digitalRead(2);
 
- if (estado && anterior == 0) {
-   encender = 1 - encender; 
+ if (estado && anterior == 0) { // si el estado es 0 se enciende el rele
+   encender = 1 - encender;   // si mi estado ya esta 1 yama la funcion de encender 
    delay (30); 
  }
  anterior = estado;
 
- if (encender)
+ if (encender)   // funcion llamada cuando es 1 
  {
- digitalWrite (3, HIGH); 
+ digitalWrite (3, HIGH); // encender el modulo rele
 
  }
-  else {
-  digitalWrite (3, LOW);
+  else {    // funcion llamada cuando es diferente a 1
+  digitalWrite (3, LOW);  // apagar el modulo rele el modulo rele
 
   }
  }
