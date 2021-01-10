28cf4457eab5d88ea25cd1a9477b762b9baf518c
fercho1997 <70421735+fercho1997@users.noreply.github.com>
2021-01-10 10:31:08 -0500

    Update sensor_pir.ino

--- sensor_pir.ino
+++ sensor_pir.ino
@@ -1,22 +1,22 @@
-int PIR = 2;
-int RELE = 3;
+int PIR = 2;  // pin 2 del arduino el sensor PIR
+int RELE = 3;  //  pin 3 del arduino para el RELE
 
 void setup() 
 {
-  pinMode(2, INPUT);
-  pinMode(3, OUTPUT);
+  pinMode(2, INPUT);  // pin 2 como entrada
+  pinMode(3, OUTPUT);   // pin 3 como salida
   Serial.begin(9600);
 }
 void loop() 
 {
-  if(digitalRead(2) == HIGH)
+  if(digitalRead(2) == HIGH)  // si el pin 2 para aun nivel alto 
   {
-    digitalWrite(3, LOW);
-    Serial.println("Sensor Activado");
+    digitalWrite(3, LOW); // encender el modulo rele
+    Serial.println("Sensor Activado"); // salir en el monitor serial Sensor Activado
   }
-  else
+  else  // si el sensor no es un nivel alto 
   {
-    digitalWrite(3, HIGH);
-    Serial.println("Modo vigilante");
+    digitalWrite(3, HIGH); // apagar el modulo rele el modulo rele
+    Serial.println("Modo vigilante");  // salir en el monitor serial Modo vigilante
   }
 }
