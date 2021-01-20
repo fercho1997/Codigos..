b34da85496cc3ffa0f9782253648ac5695a29bcf
fercho1997 <70421735+fercho1997@users.noreply.github.com>
2021-01-20 13:30:46 -0500

    Update poteciometro_led.ino

--- poteciometro_led.ino
+++ poteciometro_led.ino
@@ -1,14 +1,16 @@
 
-int led = 5;        
-int Poten = A0;        
-int BRILLO;
+int led = 5;        // LED en pin 5
+int Poten = A0;        // potenciometro en pin A0
+int BRILLO;           
 
 void setup(){
-  pinMode(led, OUTPUT);  
+  pinMode(led, OUTPUT);  // pin 5 como salida
+  
+	// Recuerda que las entradas analogicas no requieren inicializacion
  
 }
 
 void loop(){
-  BRILLO = analogRead(Poten)/4; 
-  analogWrite(led, BRILLO); 
+  BRILLO = analogRead(Poten)/4;  // valor leido de entrada analogica divido por 4 ya que el arduino lee de 0 a 250 y el Potenciómetro es de 100k
+  analogWrite(led, BRILLO);    // brillo del LED proporcional al giro del potenciometro
 }
